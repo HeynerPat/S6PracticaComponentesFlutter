2033ba08e7704d027bde7388608177c73a75eebf
HeynerPat <72895524+HeynerPat@users.noreply.github.com>
2022-02-19 10:08:39 -0500

    Image y Fadein Image

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -9,7 +9,13 @@ class CardPage extends StatelessWidget {
       ),
       body: ListView(
         padding: EdgeInsets.all(10.0),
-        children: <Widget>[_cardTipo1()],
+        children: <Widget>[
+          _cardTipo1(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo2()
+        ],
       ),
     );
   }
@@ -37,6 +43,28 @@ class CardPage extends StatelessWidget {
               ),
             ],
           )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo2() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          FadeInImage(
+            image: NetworkImage(
+                'https://th.bing.com/th/id/R.e8c2326f2c8368bb3137720155f6b8a4?rik=Cy6w6SA5uSG4iw&riu=http%3a%2f%2fwallup.net%2fwp-content%2fuploads%2f2016%2f03%2f10%2f340008-nature-landscape-mountains-sunset-clouds-sunlight-pond-grass-sky-Italy.jpg&ehk=mn4qSabd3cnZhTA1oLk7XJNzQLzX5dwQhK3jQAKwF38%3d&risl=&pid=ImgRaw&r=0'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            fadeInDuration: Duration(milliseconds: 200),
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+          /*Image(
+            image: NetworkImage(
+                'https://th.bing.com/th/id/R.e8c2326f2c8368bb3137720155f6b8a4?rik=Cy6w6SA5uSG4iw&riu=http%3a%2f%2fwallup.net%2fwp-content%2fuploads%2f2016%2f03%2f10%2f340008-nature-landscape-mountains-sunset-clouds-sunlight-pond-grass-sky-Italy.jpg&ehk=mn4qSabd3cnZhTA1oLk7XJNzQLzX5dwQhK3jQAKwF38%3d&risl=&pid=ImgRaw&r=0'),
+          ), */
+          Container(padding: EdgeInsets.all(10.0), child: Text('No tengo idea'))
         ],
       ),
     );
