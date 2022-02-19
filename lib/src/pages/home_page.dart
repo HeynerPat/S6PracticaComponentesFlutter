d8d72b3093d8def488aed10748c20b31c14a7f31
HeynerPat <72895524+HeynerPat@users.noreply.github.com>
2022-02-19 00:32:05 -0500

    Future Builder

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -13,25 +13,37 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _lista() {
-    print(menuProvider.opciones);
-    return ListView(
-      children: _listaItems(),
+    //menuProvider.cargarData();
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _listaItems(snapshot.data!),
+        );
+      },
     );
   }
 
-  List<Widget> _listaItems() {
-    return [
-      ListTile(
-        title: Text('Hola Mundo'),
-      ),
-      Divider(),
-      ListTile(
-        title: Text('Hola Mundo'),
-      ),
-      Divider(),
-      ListTile(
-        title: Text('Hola Mundo'),
-      ),
-    ];
+  List<Widget> _listaItems(List<dynamic> data) {
+    final List<Widget> opciones = [];
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: Icon(
+          Icons.account_circle,
+          color: Colors.green,
+        ),
+        trailing: Icon(
+          Icons.keyboard_arrow_right,
+          color: Colors.green,
+        ),
+        onTap: () {},
+      );
+      opciones
+        ..add(widgetTemp)
+        ..add(Divider());
+    });
+    return opciones;
   }
 }
