07341fdd3052f690ba6a830067be852f6cc12483
HeynerPat <72895524+HeynerPat@users.noreply.github.com>
2022-02-19 08:05:05 -0500

    Navegar con rutas con nombre

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:componentes/src/pages/home_page.dart';
 //import 'package:componentes/src/pages/home_temp.dart';
+import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/pages/avatar_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,8 +10,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Componentes App',
-        debugShowCheckedModeBanner: false,
-        home: HomePage());
+      title: 'Componentes App',
+      debugShowCheckedModeBanner: false,
+      //home: HomePage()
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) => HomePage(),
+        'alert': (BuildContext context) => AlertPage(),
+        'avatar': (BuildContext context) => AvatarPage(),
+      },
+    );
   }
 }
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -38,8 +38,9 @@ class HomePage extends StatelessWidget {
           color: Colors.green,
         ),
         onTap: () {
-          final route = MaterialPageRoute(builder: (context) => AlertPage());
-          Navigator.push(context, route);
+          //final route = MaterialPageRoute(builder: (context) => AlertPage());
+          //Navigator.push(context, route);
+          Navigator.pushNamed(context, opt['ruta']);
         },
       );
       opciones
