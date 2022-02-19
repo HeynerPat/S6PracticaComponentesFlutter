2cc75852ecbd7422a60b5fed7062f09b38c79a1c
HeynerPat <72895524+HeynerPat@users.noreply.github.com>
2022-02-19 08:50:20 -0500

    Rutas generadas automaticamente y archivo de rutas independiente

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:componentes/src/pages/home_page.dart';
+//import 'package:componentes/src/pages/home_page.dart';
 //import 'package:componentes/src/pages/home_temp.dart';
 import 'package:componentes/src/pages/alert_page.dart';
-import 'package:componentes/src/pages/avatar_page.dart';
+//import 'package:componentes/src/pages/avatar_page.dart';
+import 'package:componentes/src/routes/routes.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,10 +15,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       //home: HomePage()
       initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/': (BuildContext context) => HomePage(),
-        'alert': (BuildContext context) => AlertPage(),
-        'avatar': (BuildContext context) => AvatarPage(),
+      routes: getApplicationRoutes(),
+      onGenerateRoute: (RouteSettings settings) {
+        return MaterialPageRoute(
+            builder: (BuildContext context) => AlertPage());
       },
     );
   }
